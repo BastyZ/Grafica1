d2b7451a0fae52174dc7a2acdfebc0907769ba08
BastyZ <bastian.inostroza@gmail.com>
2018-04-05 17:47:05 -0300

    initial distances settled

--- src/fabrica.py
+++ src/fabrica.py
@@ -2,6 +2,7 @@
 """
 Esto inicia y ejecuta la Tarea
 @version 1.1
+@author Bastián Inostroza
 """
 
 # Importar librerías
@@ -9,7 +10,7 @@ import matplotlib.pyplot as plt  # grafico
 import tqdm
 import numpy as np
 
-RRR = 577;
+RRR = 577
 
 class Corte:
     def __init__(self, dh):
@@ -17,3 +18,14 @@ class Corte:
         Constructor
         :param dh: Tamaño de la Grilla
         """
+
+        # Distancias fijas (metros)
+        self.ancho = 4000
+        self.alto = 2000
+        self.ancho_playa = 400
+        self.ancho_fabrica = 120
+
+        # Distancias relativas por grilla
+        self.dh = dh
+        self._h = int(float(self.ancho) / self.dh)
+        self._w = int(float(self.alto) / self.dh)
