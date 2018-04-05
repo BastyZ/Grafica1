d4772c9fba5114fac3a25d1a0ac4635eef5c7632
BastyZ <bastian.inostroza@gmail.com>
2018-04-05 17:33:04 -0300

    importadas librerias y creado constructor con info

--- src/fabrica.py
+++ src/fabrica.py
@@ -1,5 +1,19 @@
 # coding=utf-8
 """
-Esto inicia y ejecu
+Esto inicia y ejecuta la Tarea
 @version 1.1
 """
+
+# Importar librerías
+import matplotlib.pyplot as plt  # grafico
+import tqdm
+import numpy as np
+
+RUT = 577;
+
+class Corte:
+    def __init__(self, dh):
+        """"
+        Constructor
+        :param dh: Tamaño de la Grilla
+        """
