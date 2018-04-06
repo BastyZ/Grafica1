d87a5a70b68d29fdd0f80e700958743e8e714d86
BastyZ <bastian.inostroza@gmail.com>
2018-04-06 10:46:11 -0300

    created function to draw Bresenham's lines on a matrix

--- src/fabrica.py
+++ src/fabrica.py
@@ -30,7 +30,58 @@ class Corte:
         self._h = int(float(self.ancho) / self.dh)
         self._w = int(float(self.alto) / self.dh)
 
+# Estudiar linea Bresenham
+    def bresenham_line(matrix, x1, y1, x2, y2):
+        """"
+        Tiralinea de Bresenham
+          La idea es que lance las lineas para cualquier matriz, por lo que
+          debe darsele los valores incluyendo el dh correspondiente, mejor
+          dicho, las distancias escaladas. Creado a partir del pseudocodigo
+          de Wikipedia
+        """
+        dy = y2 - y1
+        dx = x2 - x1
+        x = x1
+        y = y1
+
+        # incremento para avance inclinado
+        inc_yi = dy / abs(dy)
+        dy = abs(dy)
+        inc_xi = dx / abs(dx)
+        dx = abs(dx)
+
+        # incremento para avance recto
+        if dx >= dy:
+            inc_yr = 0
+            inc_xr = inc_xi
+        else:
+            inc_yr = inc_yi
+            inc_xr = 0
+            # dy > dx por lo que se intercambian
+            k = dx
+            dx = dy
+            dy = k
+
+        # inicialización de valores
+        av_r = 2*dy
+        av = av_r - dx
+        av_i = av - dx
+
+        # trazado de linea
+        while x != x2:
+            matrix[x][y] = 1
+            if av >= 0:
+                # avance inclinado
+                x += inc_xi
+                y += inc_yi
+                av += av_i
+            else:
+                # avance recto
+                x += inc_xr
+                y += inc_yr
+                av += av_r
 
         # Calculo donde está la linea de la montaña,
         # luego desde allí hacia abajo declarar los valores fijos de la montaña
         # y sus contornos
+
